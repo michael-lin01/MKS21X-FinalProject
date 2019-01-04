2ce2ee5b20fff476c9e941e631e4569c1841e204
mlin01 <mlin01@stuy.edu>
2019-01-04 16:15:11 -0500

    move method

--- Plane.java
+++ Plane.java
@@ -5,11 +5,37 @@ public class Plane{
   private boolean isAtHome;
   private int pointValue;
   private boolean hasReachedEnd;
+  private int xcor;
+  private int ycor;
+  private String direction;
 
   public Plane(String color){
     this.color = color;
     isAtHome = true;
     hasReachedEnd = false;
+  }
+  
+  private void move(int numTiles){
+    if(direction.equals("N")) ycor -= 1;
+    if(direction.equals("S")) ycor += 1;
+    if(direction.equals("E")) xcor += 1;
+    if(direction.equals("W")) xcor -= 1;
+    if(direction.equals("NE")) {
+      ycor -= 1;
+      xcor += 1;
+    }
+    if(direction.equals("NW")) {
+      ycor -= 1;
+      xcor -= 1;
+    }
+    if(direction.equals("SW")) {
+      ycor += 1;
+      xcor -= 1;
+    }
+    if(direction.equals("SE")) {
+      ycor += 1;
+      xcor += 1;
+    }
   }
 
 }
