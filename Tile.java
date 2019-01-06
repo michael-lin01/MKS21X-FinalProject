eefe22beba0f1f0d644b064d280701f456c166fd
Michael Lin <mlin01@stuy.edu>
2019-01-06 17:34:12 -0500

    added accessor and mutator methods to tile

--- Tile.java
+++ Tile.java
@@ -1,11 +1,40 @@
 public class Tile{
 
   private String color;
-  private boolean isPlaneHere;
-  private boolean isDangerZone;
-  private boolean isShortcut;
+  private boolean PlaneHere;
+  private boolean DangerZone;
+  private boolean Shortcut;
 
   public Tile(String color){
-    isPlaneHere = false;
+    PlaneHere = false;
   }
+
+  public void setPlaneHere(boolean b){
+    PlaneHere = b;
+  }
+
+  public void setDangerZone(boolean b){
+    DangerZone = b;
+  }
+
+  public void setShortcut(boolean b){
+    Shortcut = b;
+  }
+
+  public String getColor(){
+    return color;
+  }
+
+  public boolean isPlaneHere(){
+    return PlaneHere;
+  }
+
+  public boolean isDangerZone(){
+    return DangerZone;
+  }
+
+  public boolean isShortcut(){
+    return Shortcut;
+  }
+  
 }
